18c0af2bc0c5a1aca614e77536f2c7944de48e6b
toy80 <12060052+toy80@user.noreply.github.com>
2020-08-22 21:16:57 +0800

    refactor for checkptr

--- helper.go
+++ helper.go
@@ -8,6 +8,8 @@ import (
 	"unsafe"
 )
 
+type sliceHeader = reflect.SliceHeader
+
 func CStrOrNil(s string) (c *int8, free func()) {
 	if s == "" {
 		return nil, func() {}
@@ -22,7 +24,11 @@ func CStr(s string) (c *int8, free func()) {
 		panic("failed to allocate unmanaged memory")
 	}
 
-	a := (*((*[0x7FFFFFFF]byte)(p)))[:n]
+	//a := (*((*[0x7FFFFFFF]byte)(p)))[:n]
+	var a []byte
+	h := (*sliceHeader)(unsafe.Pointer(&a))
+	h.Data, h.Len, h.Cap = uintptr(p), int(n), int(n)
+
 	copy(a, []byte(s))
 	a[n-1] = 0
 
@@ -46,7 +52,11 @@ func CStrSlice(ss []string) (c **int8, n uint32, free func()) {
 		if p == nil {
 			panic("failed to allocate unmanaged memory")
 		}
-		a := (*((*[0x7FFFFFFF]*int8)(p)))[:n]
+		// a := (*((*[0x7FFFFFFF]*int8)(p)))[:n]
+		var a []*int8
+		h := (*sliceHeader)(unsafe.Pointer(&a))
+		h.Data, h.Len, h.Cap = uintptr(p), int(n), int(n)
+
 		frees = make([]func(), n)
 		for i := range a {
 			a[i], frees[i] = CStr(ss[i])
@@ -63,35 +73,21 @@ func CStrSlice(ss []string) (c **int8, n uint32, free func()) {
 }
 
 func ptrInt8ToString(p *int8) string {
-	if p == nil || *p == 0 {
-		return ""
-	}
-	usp := unsafe.Pointer(p)
-	pp := (*[0x7FFFFFFF]byte)(usp)
-	s := pp[:]
-	for i, v := range s {
-		if v == 0 {
-			tmp := make([]byte, i)
-			copy(tmp, s)
-			return string(tmp)
-		}
-	}
-	return ""
+	return ptrUint8ToString((*uint8)(unsafe.Pointer(p)))
 }
 
 func ptrUint8ToString(p *uint8) string {
 	if p == nil || *p == 0 {
 		return ""
 	}
-	s := (*(*[0x7FFFFFFF]byte)(unsafe.Pointer(p)))[:]
-	for i, v := range s {
-		if v == 0 {
-			tmp := make([]byte, i)
-			copy(tmp, s)
-			return string(tmp)
-		}
+	var n uintptr
+	for *(*int8)(unsafe.Pointer(uintptr(unsafe.Pointer(p)) + n)) != 0 {
+		n++
 	}
-	return ""
+	var s []byte
+	h := (*sliceHeader)(unsafe.Pointer(&s))
+	h.Data, h.Len, h.Cap = uintptr(unsafe.Pointer(p)), int(n), int(n)
+	return string(s)
 }
 
 func GoStr(x interface{}) string {
@@ -145,7 +141,11 @@ func GoStrSlice(pp **int8, n uint32) (ss []string) {
 		return
 	}
 	ss = make([]string, 0, n)
-	a := (*((*[0x7FFFFFFF]*int8)(unsafe.Pointer(pp))))[:n]
+	// a := (*((*[0x7FFFFFFF]*int8)(unsafe.Pointer(pp))))[:n]
+	var a []*int8
+	h := (*sliceHeader)(unsafe.Pointer(&a))
+	h.Data, h.Len, h.Cap = uintptr(unsafe.Pointer(pp)), int(n), int(n)
+
 	for _, p := range a {
 		ss = append(ss, GoStr(p))
 	}
@@ -165,7 +165,16 @@ func CUint32Array(s []uint32) (c *uint32, n uint32, free func()) {
 	if p == nil {
 		panic("failed to allocate unmanaged memory")
 	}
-	copy((*((*[0x7FFFFFFF]uint32)(p)))[:n], s)
+
+	// copy((*((*[0x7FFFFFFF]uint32)(p)))[:n], s)
+	var ds []uint32
+	hd := (*sliceHeader)(unsafe.Pointer(&ds))
+	hd.Data, hd.Len, hd.Cap = uintptr(p), int(n), int(n)
+	if len(ds) != int(n) {
+		panic("internal error")
+	}
+	copy(ds, s)
+
 	return (*uint32)(p), n, func() {
 		MemFree(p)
 	}
@@ -184,7 +193,14 @@ func CUint16Array(s []uint16) (c *uint16, n uint32, free func()) {
 	if p == nil {
 		panic("failed to allocate unmanaged memory")
 	}
-	copy((*((*[0x7FFFFFFF]uint16)(p)))[:n], s)
+	// copy((*((*[0x7FFFFFFF]uint16)(p)))[:n], s)
+	var ds []uint16
+	hd := (*sliceHeader)(unsafe.Pointer(&ds))
+	hd.Data, hd.Len, hd.Cap = uintptr(p), int(n), int(n)
+	if len(ds) != int(n) {
+		panic("internal error")
+	}
+	copy(ds, s)
 	return (*uint16)(p), n, func() {
 		MemFree(p)
 	}
@@ -203,7 +219,14 @@ func CFloat32Array(s []float32) (c *float32, n uint32, free func()) {
 	if p == nil {
 		panic("failed to allocate unmanaged memory")
 	}
-	copy((*((*[0x7FFFFFFF]float32)(p)))[:n], s)
+	// copy((*((*[0x7FFFFFFF]float32)(p)))[:n], s)
+	var ds []float32
+	hd := (*sliceHeader)(unsafe.Pointer(&ds))
+	hd.Data, hd.Len, hd.Cap = uintptr(p), int(n), int(n)
+	if len(ds) != int(n) {
+		panic("internal error")
+	}
+	copy(ds, s)
 	return (*float32)(p), n, func() {
 		MemFree(p)
 	}
@@ -222,7 +245,11 @@ func CByteArray(s []byte) (c *byte, n uint32, free func()) {
 	if p == nil {
 		panic("failed to allocate unmanaged memory")
 	}
-	copy((*((*[0x7FFFFFFF]byte)(p)))[:n], s)
+	// copy((*((*[0x7FFFFFFF]byte)(p)))[:n], s)
+	var ds []byte
+	hd := (*sliceHeader)(unsafe.Pointer(&ds))
+	hd.Data, hd.Len, hd.Cap = uintptr(p), int(n), int(n)
+	copy(ds, s)
 	return (*byte)(p), n, func() {
 		MemFree(p)
 	}
@@ -284,7 +311,21 @@ func LoadInstanceProc(instance Instance, ppfn interface{}) error {
 }
 
 func MemCopy(dst, src unsafe.Pointer, size uint64) int {
-	return copy(((*[0x7FFFFFFF]byte)(dst))[:size], ((*[0x7FFFFFFF]byte)(src))[:size])
+	// *[0x7FFFFFFF]byte 这种模式通不过checkptr:
+	// var a [10]byte
+	// ((*[0x7FFFFFFF]byte)(unsafe.Pointer(&a)))[1] = 1
+	//
+	// 我们换一种(不安全的)方式构造slice:
+
+	var ds []byte
+	hd := (*sliceHeader)(unsafe.Pointer(&ds))
+	hd.Data, hd.Len, hd.Cap = uintptr(dst), int(size), int(size)
+
+	var ss []byte
+	hs := (*sliceHeader)(unsafe.Pointer(&ss))
+	hs.Data, hs.Len, hs.Cap = uintptr(src), int(size), int(size)
+
+	return copy(ds, ss)
 }
 
 var ShaderFileTypes = map[string]ShaderStageFlags{
